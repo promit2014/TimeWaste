14ad1242129da7f277471df30b00486c0c3698f3
promit2014 <promit.2014@gmail.com>
2016-10-30 14:32:51 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+mongoose.connect('mongodb://localhost:27017/time-waste');
+
+app.use('/app', express.static(__dirname+'/app'));
+app.use('/bower_components',express.static(__dirname+'/bower_components'));
+
+app.get('/',function(req,res){
+	//res.sendFile('index.html');
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.listen('8081',function(){
+	console.log("Listening for localhost 3000");
+});
