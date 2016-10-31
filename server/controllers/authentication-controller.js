1cdf35a7a9870db7cf1422f7a9c02603738ca606
promit2014 <promit.2014@gmail.com>
2016-10-31 14:50:05 +0530

    fixed login function issue

--- server/controllers/authentication-controller.js
+++ server/controllers/authentication-controller.js
@@ -5,10 +5,10 @@ var signup = function(req,res){
 	var user = new User(req.body);
 	user.save(function(err, data){
 		if(err){
-			console.log("error was there", err);
+			//console.log("error was there", err);
 			res.status(500).json({"error":"Error Occured"});
 		}else{
-			console.log("data success fully saved",data);			
+			//console.log("data success fully saved",data);			
 			res.status(201).json({"success":"created successfully"});
 		}
 	});
@@ -19,12 +19,16 @@ var login = function(req,res){
 		if(err){
 			console.log("Error Out",err);
 			res.status(500).json({error:"Error Occured"});
-		}else if(results.length  = 0){
+		};
+
+		if(results.length  === 0){
 			console.log("No User Exist");
-			res.status(200).json({error : "Not Found"});
-		}else if(results.length > 0){
+			res.status(401).json({error : "Not Found"});
+		};
+
+		if(results.length === 1){
 			console.log("Successfully found User");
-			res.status(200).json({email : req.body.email});
+			res.status(200).json({email : results[0].email});
 		}
 	});
 }
