6596c54124536506cceb614d5846507eb20c8054
promit2014 <promit.2014@gmail.com>
2016-11-18 22:10:28 +0530

    implemented new view sign in

--- app/app.js
+++ app/app.js
@@ -1,11 +1,20 @@
 
 (function(){
 	angular.module('TimeWaste',['ui.router','btford.socket-io'])
-	.config(['$stateProvider',function($stateProvider) {
+	.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider) {
+		
+		$urlRouterProvider.otherwise('/signin');
+
 		$stateProvider.state('signup',{
 			url:"/signup",
 			templateUrl: "/signup/templates/signup.html",
 			controller: "SignupController"
 		});
+
+		$stateProvider.state('signin',{
+			url:"/signin",
+			templateUrl: "/signin/templates/signin.html"
+		});
+
 	}]);
 }());
--- app/appLayout/controllers/navCtrl.js
+++ app/appLayout/controllers/navCtrl.js
@@ -1,7 +1,7 @@
 angular.module('TimeWaste')
 .controller('navCtrl', ['$state','$scope' , '$http' , 'socket', function($state,$scope,$http,socket){
-	$scope.logUserIn = function(){
-		$http.post('/api/user/login', $scope.user).success(function(response){
+	$scope.$parent.logUserIn = function(user){
+		$http.post('/api/user/login', user).success(function(response){
 			console.log("successfully logged in",response);
 			localStorage.setItem('User-Data',JSON.stringify(response));
 			socket.connect();
